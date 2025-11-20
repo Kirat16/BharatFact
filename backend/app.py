f4bd139ab652ab3d30887d377bfd009a305f507f
Kirat16 <bluekirat@gmail.com>
2025-11-20 10:16:46 +0530

    Change

--- backend/app.py
+++ backend/app.py
@@ -143,4 +143,4 @@ def predict(inp: PredictIn, th: float | None = None):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
